b905191191948922a9ff6e21e5fb94931887e64b
samiragarwala <samir.aggarwala@gmail.com>
2024-06-05 02:37:37 +0000

    adding eval code for bert baseline

--- DiscordBot/eval_bert_baseline.py
+++ DiscordBot/eval_bert_baseline.py
@@ -10,6 +10,19 @@ import datasets
 from transformers import AutoTokenizer, BertForSequenceClassification, Trainer, TrainingArguments
 import evaluate
 import os
+from safetensors.torch import load_file
+from collections import defaultdict
+import json
+from tqdm import tqdm
+
+ckpt_dir = "./results/checkpoint-4000"
+output_file = "classifier_outputs.json"
+num_save = 50
+
+if torch.cuda.is_available():
+    device = 'cuda'
+else:
+    device = 'cpu'
 
 # adapted from https://huggingface.co/docs/transformers/en/training
 metric = evaluate.combine(["accuracy", "f1", "precision", "recall"])
@@ -44,40 +57,37 @@ model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_l
 model.classifier = nn.Sequential(nn.Linear(768, 512), nn.ReLU(), nn.Linear(512, 256), nn.ReLU(), nn.Linear(256, 128), nn.ReLU(), nn.Linear(128, 2))
 model.to(DEVICE)
 
-# freezing pretrained BERT encoder
-for param in model.base_model.parameters():
-    param.requires_grad = False
+model_path = os.path.join(ckpt_dir, "model.safetensors")
+weights = load_file(model_path, "cuda")
 
-print("Printing model layers with gradient setting...")
+# loading weights from checkpoint
+print("Loading weights from checkpoint %s" % (model_path))
 for name, param in model.named_parameters():
-    print(name, param.requires_grad)
+    to_load_weight = weights[name]
+    model.state_dict()[name].data.copy_(to_load_weight)
 
-training_args = TrainingArguments(
-    output_dir="./results",
-    evaluation_strategy="epoch",
-    per_device_train_batch_size=16,
-    per_device_eval_batch_size=16,
-    num_train_epochs=4,
-    weight_decay=0.01,
-    logging_steps=100,
-    learning_rate=0.001,
-    save_steps=2000
-)
+outputs = defaultdict(dict)
+tot = 0.0
+correct = 0.0
+for i in tqdm(range(len(tokenized_dataset["test"]))):
+    outputs[i]["message"] = tokenized_dataset["test"]["text"][i]
+    outputs[i]["gt"] = int(tokenized_dataset["test"]["labels"][i].cpu())
 
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=tokenized_dataset["train"],
-    eval_dataset=tokenized_dataset["test"],
-    compute_metrics=compute_metrics
-)
+    input_ids = tokenized_dataset["test"]["input_ids"][i].unsqueeze(0).to(device)
+    attention_mask = tokenized_dataset["test"]["attention_mask"][i].unsqueeze(0).to(device)
+    labels =  tokenized_dataset["test"]["labels"][i].unsqueeze(0).to(device)
+    token_type_ids = torch.tensor(tokenized_dataset["test"]["token_type_ids"][i]).unsqueeze(0).to(device)
 
-print("Training model")
-trainer.train(resume_from_checkpoint = True)
-trainer.save_model("final_model")
+    outputs[i]["prediction"] = int(torch.argmax(model(input_ids, attention_mask, token_type_ids)["logits"]).cpu())
 
+    correct += (outputs[i]["gt"] == outputs[i]["prediction"])
+    tot += 1.0
 
+    if i % num_save == 0 and i != 0:
+        with open(output_file, 'w') as f:
+            json.dump(outputs, f, indent=6)      
 
-print("Running evaluation")
-results = trainer.evaluate()
-print(results)
+with open(output_file, 'w') as f:
+    json.dump(outputs, f, indent=6)
+
+print("Accuracy: %.2f" % (correct / tot))
